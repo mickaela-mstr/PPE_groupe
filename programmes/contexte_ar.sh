52c17c4a9b1ad8236a9e559c08f19cceabb3bb74
mickaela-mstr <mickaelam6@gmail.com>
2024-12-28 20:21:54 +0100

    ajout tableaux ar et fr + contxt ar

--- programmes/contexte_ar.sh
+++ programmes/contexte_ar.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mot_etudie="legend"
-input_file="../dumps-text/anglais.txt"
-output_file="../contextes/anglais.txt"
+mot_etudie="أسطورة"
+input_file="../dumps-text/arabe.txt"
+output_file="../contextes/arabe.txt"
 
 grep -i -w -C 1 "$mot_etudie" "$input_file" | sed "s/\b$mot_etudie\b/>>>$mot_etudie<<</Ig" > "$output_file"
 
