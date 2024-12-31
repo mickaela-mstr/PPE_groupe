f96ee5ec4c62328aab0ffe267e7d70ba240ea3e2
mickaela-mstr <mickaelam6@gmail.com>
2024-12-31 20:53:53 +0100

    modif pals

--- programmes/make_pals_corpus.sh
+++ programmes/make_pals_corpus.sh
@@ -1,41 +1,16 @@
 #!/bin/bash
 
-# Vérification des arguments
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <type_fichier>"
-    echo "Exemple: $0 dumps-text ou $0 contextes"
-    exit 1
+if [ $# -ne 2 ] #verifier si le script a un argument
+then
+	echo "ce programme demande deux arguments: un argument pour le chemin vers le dossier et la langue"
 fi
 
-# Argument : type de fichier à traiter (dumps-text ou contextes)
-type_fichier="$1"
-
-# Dossiers et langues
-dossier_pals="pals"
-dossier_source="$type_fichier"
-langues=("arabe" "français" "anglais")
-
-# Création du dossier pals s'il n'existe pas
-mkdir -p "$dossier_pals"
-
-# Boucle sur les langues
-for langue in "${langues[@]}"; do
-    fichier_source="$dossier_source/$langue.txt"
-    fichier_sortie="$dossier_pals/${type_fichier}-${langue}.txt"
-
-    echo "Traitement des fichiers $type_fichier pour la langue $langue..."
-
-    # Vérification de l'existence du fichier source
-    if [[ -f "$fichier_source" ]]; then
-        echo "Traitement de $fichier_source..."
-        # Prétraitement : suppression des caractères non alphanumériques, découpage en mots
-        cat "$fichier_source" | sed 's/[^[:alnum:] ]//g' | tr ' ' '\n' | sed '/^$/d' > "$fichier_sortie"
-        echo "Fichier généré : $fichier_sortie"
-    else
-        echo "Fichier introuvable : $fichier_source"
-        > "$fichier_sortie"  # Crée un fichier vide si la source est absente
-        echo "Fichier vide créé : $fichier_sortie"
-    fi
+dossier=$1
+langue=$2
+for file in "../$dossier/$langue/$langue-*.txt"; do
+    cat $file | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g"  > ../pals2/dumps-text/$langue
 done
 
-
+for file in "../contextes/$langue/$langue-*.txt"; do
+    cat $file | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" > ../pals2/contextes/$langue
+done
