b9d776995ed38c10a2d53ed31fda6137025b7e8b
mickaela-mstr <mickaelam6@gmail.com>
2025-01-08 23:52:00 +0100

    essai régler pb encodage fr et ar

--- programmes/make_pals_corpus2.sh
+++ programmes/make_pals_corpus2.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+export LANG=C.UTF-8 #merci kehina
+export LC_ALL=C.UTF-8
+
+if [ $# -ne 1 ] #verifier si le script a un argument
 then
-    echo "Ce programme demande un argument =  la langue."
-    exit 1
+	echo "ce programme demande un argument: un argument pour la langue"
 fi
 
 langue=$1
 
-for file in ../dumps-text/$langue/$langue-*.txt; do
-    perl -CSD -pe 's/[^\p{L}\p{M}\p{N}\p{Z}\.]+/\n/g' $file > ../pals/dumps-text/$langue.txt
-done
+if [ "$langue" == "arabe" ]; then
+    for file in "../dumps-text/$langue/$langue-*.txt"; do
+        cat $file | tr -cs "ا-ي." "\n" | sed "s/\./\n/g"  > ../pals/dumps-text/$langue.txt
+        done
+    for file in "../contextes/$langue/$langue-*.txt"; do
+        cat $file | tr -cs "ا-ي." "\n" | sed "s/\./\n/g" > ../pals/contextes/$langue.txt
+        done
+fi
 
-for file in ../contextes/$langue/$langue-*.txt; do
-    perl -CSD -pe 's/[^\p{L}\p{M}\p{N}\p{Z}\.]+/\n/g' $file > ../pals/contextes/$langue.txt
-done
+if [ "$langue" == "francais" ]; then
+    for file in "../dumps-text/$langue/$langue-*.txt"; do
+        cat $file | tr -cs "a-zàâçéèêëîïôûùüÿñæœ" "\n" | sed "s/\./\n/g"  > ../pals/dumps-text/$langue.txt
+        done
+    for file in "../contextes/$langue/$langue-*.txt"; do
+        cat $file | tr -cs "a-zàâçéèêëîïôûùüÿñæœ" "\n" | sed "s/\./\n/g" > ../pals/contextes/$langue.txt
+        done
+fi
+
+if [ "$langue" == "anglais" ]; then
+
+    for file in "../dumps-text/$langue/$langue-*.txt"; do
+        cat $file | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g"  > ../pals/dumps-text/$langue.txt
+        done
+
+    for file in "../contextes/$langue/$langue-*.txt"; do
+        cat $file | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" > ../pals/contextes/$langue.txt
+        done
+fi
