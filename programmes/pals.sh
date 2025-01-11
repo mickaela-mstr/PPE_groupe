30ebd88fe4bdad5499095930efe10209952b1790
mickaela-mstr <mickaelam6@gmail.com>
2025-01-11 20:07:54 +0100

    réglage pb run_all.sh + retrait des urls non fonctionnels

--- programmes/pals.sh
+++ programmes/pals.sh
@@ -5,30 +5,26 @@ EN_CONTEXT_DIR="$BASE_DIR/contextes/anglais"
 EN_DUMPS_DIR="$BASE_DIR/dumps-text/anglais"
 PALS_DIR="$BASE_DIR/pals"
 
-# Arguments pour le script
-input_folder=$1       # Dossier contenant les fichiers à traiter
-base_name=$2          # Nom de base pour les fichiers de sortie
+input_folder=$1
+base_name=$2
 
-# Fonction de traitement des fichiers
 process_file() {
     input_file=$1
     output_file=$2
 
-    # Tokenisation : un mot par ligne, suppression des lignes vides
     cat $input_file | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" > "$output_file"
 }
 
-# Vérification et assignation du bon dossier d'entrée
 if [[ $input_folder == "dumps-text" ]]; then
     input_folder=$EN_DUMPS_DIR
 elif [[ $input_folder == "contextes" ]]; then
     input_folder=$EN_CONTEXT_DIR
 else
-    echo "Dossier non reconnu. Veuillez utiliser : ar_dumps, fr_dumps, ar_context ou fr_context."
+    echo "Dossier non reconnu."
     exit 1
 fi
 
-# Vérifier si le dossier d'entrée contient des fichiers
+
 if [[ ! -d $input_folder ]]; then
     echo "Erreur : Le dossier $input_folder n'existe pas."
     exit 1
@@ -39,11 +35,8 @@ if [[ -z $(ls "$input_folder"/*.txt 2>/dev/null) ]]; then
     exit 1
 fi
 
-# Boucle pour traiter les fichiers dans le dossier donné
 for file in "$input_folder"/*.txt; do
     filename=$(basename "$file" .txt)  # Nom du fichier sans extension
     output_file="$PALS_DIR/${base_name}-${filename}.txt"
     process_file "$file" "$output_file"
 done
-
-echo "Traitement terminé. Les fichiers PALS sont générés dans $PALS_DIR."
--- programmes/run_all.sh
+++ programmes/run_all.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Définition des noms des scripts dans l'ordre d'exécution
 scripts=(
     "aspiration_all.sh"
     "dump_all.sh"
@@ -9,16 +8,13 @@ scripts=(
     "tableau_all.sh"
 )
 
-# Vérification des droits d'exécution et exécution des scripts
 for script in "${scripts[@]}"; do
     if [[ -f "$script" ]]; then
-        # Donner les droits d'exécution si nécessaire
         chmod +x "$script"
 
         echo "Exécution de $script..."
         ./"$script"
 
-        # Vérifier si le script s'est exécuté avec succès
         if [[ $? -ne 0 ]]; then
             echo "Erreur lors de l'exécution de $script. Arrêt."
             exit 1
@@ -29,4 +25,3 @@ for script in "${scripts[@]}"; do
     fi
 done
 
-echo "Tous les scripts ont été exécutés avec succès."
