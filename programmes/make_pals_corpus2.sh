d64d871ab7131a36f17b3d12774a6712ad83b560
Zeinab Omar <zeinabomar@MacBook-Air-de-Zeinab.local>
2025-01-12 03:36:50 +0100

    update

--- programmes/make_pals_corpus2.sh
+++ programmes/make_pals_corpus2.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
-export LANG=C.UTF-8 #merci kehina
+export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
 
-if [ $# -ne 1 ] #verifier si le script a un argument
-then
-    echo "ce programme demande un argument: un argument pour la langue"
+if [ $# -ne 1 ]; then
+    echo "Ce programme demande un argument : un argument pour la langue"
+    exit 1
 fi
 
 langue=$1
 
 if [ "$langue" == "arabe" ]; then
-    for file in "../dumps-text/$langue/$langue-*.txt"; do
-        cat $file | tr -cs "ا-ي." "\n" | sed "s/\./\n/g"  > ../pals/dumps-text/$langue.txt
-        done
-    for file in "../contextes/$langue/$langue-*.txt"; do
-        cat $file | tr -cs "ا-ي." "\n" | sed "s/\./\n/g" > ../pals/contextes/$langue.txt
-        done
+    # Pour les fichiers dumps-text
+    for file in ../pals/dumps-text/$langue*.txt; do
+        cat "$file" | tr -cs '\u0600-\u06FF' '\n' | sed "s/\./\n/g" >> "../pals/cooccurrent/dumps-text/arabe.txt"
+    done
+    # Pour les fichiers contextes
+    for file in ../pals/contextes/$langue*.txt; do
+        cat "$file" | tr -cs '\u0600-\u06FF' '\n' | sed "s/\./\n/g" >> "../pals/cooccurrent/contextes/arabe.txt"
+    done
 fi
 
 if [ "$langue" == "francais" ]; then
-    for file in "../dumps-text/$langue/$langue-*.txt"; do
-        cat $file | tr -cs "a-zàâçéèêëîïôûùüÿñæœ" "\n" | sed "s/\./\n/g"  > ../pals/dumps-text/$langue.txt
-        done
-    for file in "../contextes/$langue/$langue-*.txt"; do
-        cat $file | tr -cs "a-zàâçéèêëîïôûùüÿñæœ" "\n" | sed "s/\./\n/g" > ../pals/contextes/$langue.txt
-        done
+    for file in ../pals/dumps-text/$langue*.txt; do
+        cat "$file" | tr -cs "a-zàâçéèêëîïôûùüÿñæœ." "\n" | sed "s/\./\n/g" >> "../pals/cooccurrent/dumps-text/francais.txt"
+    done
+    for file in ../pals/contextes/$langue*.txt; do
+        cat "$file" | tr -cs "a-zàâçéèêëîïôûùüÿñæœ." "\n" | sed "s/\./\n/g" >> "../pals/cooccurrent/contextes/francais.txt"
+    done
 fi
 
 if [ "$langue" == "anglais" ]; then
-
-    for file in "../dumps-text/$langue/$langue-*.txt"; do
-        cat $file | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g"  > ../pals/dumps-text/$langue.txt
-        done
-
-    for file in "../contextes/$langue/$langue-*.txt"; do
-        cat $file | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" > ../pals/contextes/$langue.txt
-        done
+    for file in ../pals/dumps-text/$langue*.txt; do
+        cat "$file" | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" >> "../pals/cooccurrent/dumps-text/anglais.txt"
+    done
+    for file in ../pals/contextes/$langue*.txt; do
+        cat "$file" | tr -cs "[:alpha:]." "\n" | sed "s/\./\n/g" >> "../pals/cooccurrent/contextes/anglais.txt"
+    done
 fi
+
